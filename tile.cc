f250dd8ae3fee9897b3b6de8ff94a53a305ce98f
Harry Li <harryli@v1022-rn-158-234.campus-dynamic.uwaterloo.ca>
2016-07-16 21:16:42 -0400

    fixed TD

--- tile.cc
+++ tile.cc
@@ -37,108 +37,63 @@ Tile::Tile(int row, int col): row{row}, col{col} {
 			content = new Object('-' , this);
 	}else if ((row==5) && (col >= 71 && col <73)){
 			content = new Object('-' , this);
-	}else if (row==6){
-		if (col >= 74 && col <76){
+	}else if ((row==6) && (col >= 74 && col <76)){
 			content = new Object('-' , this);
-		}	
-	}else if (row== 7){
-		if (col >= 3 && col <13){
+	}else if ((row== 7) && ((col >= 3 && col <13) || (col >= 14 && col <29) || (col >= 39 && col < 43 )|| (col >= 44 && col < 60))){
 			content = new Object('-' , this);
-		}
-		if (col >= 14 && col <29){
+
+	}else if ((row== 9) && ((col >= 38 && col < 43) || (col >= 44 && col < 50)))  {
 			content = new Object('-' , this);
-		}
-		if (col >= 39 && col < 43 ){
+
+	}else if ((row== 13) && ((col >= 38 && col < 50 && col != 43) || (col >= 62 && col < 76 && col != 69))){
 			content = new Object('-' , this);
-		}
-		if (col >= 44 && col < 60){
+
+	}else if ((row== 14) && (col >= 4 && col <25 && col != 13)) {
 			content = new Object('-' , this);
-		}
-	}else if (row== 9){
-		if (col == 38 && col < 43){
+
+	}else if ((row== 15) && ( col >= 65 && col <76 && col != 69)){
 			content = new Object('-' , this);
-		}	
-		if (col == 44 && col < 50){
+
+	}else if ((row== 18) && (col >= 37 && col <64 && col != 43)){
 			content = new Object('-' , this);
-		}	
-	}else if (row== 13){
-		if (col >= 38 && col < 50 && col != 43){
+
+	}else if ((row== 22) && ((col >= 4 && col <25) || (col >= 37 && col <76))){
 			content = new Object('-' , this);
-		}
-		if (col >= 62 && col < 76 && col != 69){
-			content = new Object('-' , this);
-		}	
-	}else if (row== 14){
-		if (col >= 4 && col <25 && col != 13){
-			content = new Object('-' , this);
-		}
-	}else if (row== 15){
-		if( col >= 65 && col <76 && col != 69){
-			content = new Object('-' , this);
-		}
-	}else if (row== 18){
-		if (col >= 37 && col <64 && col != 43){
-			content = new Object('-' , this);
-		}
-	}else if (row== 22){
-		if(col >= 4 && col <25){
-			content = new Object('-' , this);
-		}
-		if (col >= 37 && col <76){
-			content = new Object('-' , this);
-		}
 	}
 	//chamber top left 
-	else if (row >= 3 && row < 29){
-		if (col >= 3 && col < 7){
+	else if ((row >= 3 && row < 7) && (col >= 3 && col < 29)){
 			content = new Object('.' , this);
-		}
 	}
 	//chamber bottom left 
-	else if (row >= 15 && row < 22){
-		if (col >= 4 && col < 25){
+	else if ((row >= 15 && row < 22) && (col >= 4 && col < 25)){
 			content = new Object('.' , this);
-		}
 	}
 	//chamber middle one 
-	else if (row >= 10 && row < 13){
-		if ( col >= 12 && col < 50){
+	else if ((row >= 10 && row < 13) && ( col >= 38 && col < 50)){
 			content = new Object('.' , this);
-		}
 	}
 	// chamber top right (40, 4) to (62, 5)
-	else if (row >= 3 && row < 5){
-		if (col >= 39 && col < 62){
+	else if ((row >= 3 && row < 5) && (col >= 39 && col < 62)){
 			content = new Object('.' , this);
-		}
-	}else if (row== 5){
-		if(col >= 39 && col < 70){
+
+	}else if ((row== 5) && (col >= 39 && col < 70)){
 			content = new Object('.' , this);
-		}
-	}else if (row== 6){
-		if (col >= 39 && col < 73){
+
+	}else if ((row== 6) && (col >= 39 && col < 73)){
 			content = new Object('.' , this);
-		}
 	}
 	//(62,8) to (76,13)
-	else if (row >=7 && row<13){
-		if (col >= 61 && col < 76){
+	else if ((row >=7 && row<13) && (col >= 61 && col < 76)){
 			content = new Object('.' , this);
-		}
 	}
 	// chamber bottom right (66,17) to (76, 19)
-	else if (row >= 16 && row < 19){
-		if (col >= 65 && col < 76){
+	else if ((row >= 16 && row < 19) && (col >= 65 && col < 76)){
 			content = new Object('.' , this);
-		}
 	}
 	//(20,38) to (22,76)
-	else if (row >= 19 && row < 22){
-		if (col >= 37 && col < 76){
+	else if ((row >= 19 && row < 22) && (col >= 37 && col < 76)){
 			content = new Object('.' , this);
-		}
 	}
-
 	else {
 		content = new Object(' ', this);
 	//	cout << "AYYYY WE MDE IT";
