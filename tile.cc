3644b025c509b9919c5cf0a1491b176d3814cb7f
Terence Tsang <terence.cn.tsang@gmail.com>
2016-07-16 22:30:54 -0400

    All tiles on stack

--- tile.cc
+++ tile.cc
@@ -4,11 +4,15 @@ using namespace std;
 
 Tile::Tile(int row, int col): row{row}, col{col} {
 	if (((col == 0 || col == 78) && (row <= 24)) || ((col == 3) && (((row >= 2) && (row <= 7)) || ((row >= 14) && (row <= 22)))) || (((col == 29) || (col == 38)) && (((row== 2) || (row == 3)) || ((row >= 5) && (row <= 7)))) || ((col == 25) && (((row >= 14) && (row <= 19)) || ((row == 21) || (row == 22)))) || ((col == 36) && (((row >= 18) && (row <= 19)) || ((row == 21) || (row == 22)))) || (((col == 37) || (col == 50)) && ((row >= 9) && (row <= 13))) || ((col == 60) && (((row >= 7) && (row <= 10)) || ((row == 12) || (row == 13)))) || ((col == 62) && (((row >= 2) && (row <= 4))))|| ((col == 64) && (((row >= 15) && (row <= 18)))) || ((col == 70) && ((row == 4) || (row == 5))) || ((col == 73) && ((row == 5) || (row == 6))) || ((col == 76)  && ((((row >= 6) && (row <= 13)))|| ((row >= 15) && (row <= 22))))) { 
-		content = new Object('|', this);
+		
+       Object temp('|', this);
+       content = &temp;
 	}
 
 	else if ((row== 0) || (row == 24)) {
-		content = new Object('-', this);
+		
+       Object temp('-', this);
+       content = &temp;
 	}
 
 	else if (((row==7 ) && (col == 13)) ||
@@ -24,7 +28,9 @@ Tile::Tile(int row, int col): row{row}, col{col} {
 		((row ==20) && (col ==36)) ||
 		((row==9 ) && (col == 43)) ||
 		((row==4) && (col == 29))) {
-		content = new Object('+', this);
+		
+       Object temp('+', this);
+       content = &temp;
 	}
 
 	else if (((row== 4) && ((col >= 30) && (col <= 37))) ||  
@@ -39,23 +45,37 @@ Tile::Tile(int row, int col): row{row}, col{col} {
 			((col == 43) && ((row >= 14) && (row <= 17))) ||
 			((col == 54) && ((row >= 12) && (row <= 15))) ||
 			((col == 69) && (row== 14))) {
-		content = new Object('#', this);
+		
+       Object temp('#', this);
+       content = &temp;
 	}
 
 	//horizontal wallz
 	else if ((row== 2) &&  ((col >= 3 && col <29) || (col >= 39 && col <62))) {
-		content = new Object('-' , this);
+		
+       Object temp('-', this);
+       content = &temp;
 	}else if ((row==4) &&  (col >= 63 && col <70)){
-			content = new Object('-' , this);
+			
+       Object temp('-', this);
+       content = &temp;
 	}else if ((row==5) && (col >= 71 && col <73)){
-			content = new Object('-' , this);
-	}else if ((row==6) && (col >= 74 && col <76)){
-			content = new Object('-' , this);
+					
+       Object temp('-', this);
+       content = &temp;
+	}else if ((row==6) && (col >= 74 && col <76)){	
+        Object temp('-', this);
+        content = &temp;
 	}else if ((row== 7) && ((col >= 3 && col <13) || (col >= 14 && col <29) || (col >= 39 && col < 43 )|| (col >= 44 && col < 60))){
-			content = new Object('-' , this);
+			
+       Object temp('-', this);
+        content = &temp;
 
 	}else if ((row== 9) && ((col >= 38 && col < 43) || (col >= 44 && col < 50)))  {
-			content = new Object('-' , this);
+                    
+            Object temp('-', this);
+            content = &temp;
+
 
 	}else if ((row== 13) && ((col >= 38 && col < 50 && col != 43) || (col >= 62 && col < 76 && col != 69))){
 			
