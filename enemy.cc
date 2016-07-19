2fcaad6a93ef5b071f5d5befa18e2081c61fa607
meghanarora <meghanarora96@gmail.com>
2016-07-19 02:56:01 -0400

    minor enemy parameter change

--- enemy.cc
+++ enemy.cc
@@ -1,6 +1,6 @@
 #include "enemy.h"
 
-Enemy:: Enemy(int HP, int Atk, int Def, std::string Race, char Kind, Tile *parent):
+Enemy:: Enemy(int HP, int Atk, int Def,char Kind, Tile *parent):
     Character(HP, Atk, Def,Race, Kind, Parent); {}
 
     
