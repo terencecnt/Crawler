d3e3d4c9b1d8cf2f8fd5a5a05681280279d8596f
Harry Li <harryli@Harrys-MacBook-Pro.local>
2016-07-19 22:27:35 -0400

    added race for player and getGold()

--- player.cc
+++ player.cc
@@ -1,6 +1,13 @@
 #include "player.h"
 
-Player::Player(int HP, int Atk, int Def, char Kind, Tile *parent): Character(HP, Atk, Def, '@', parent), gold{0} {};
+using namespace std;
+
+Player::Player(int HP, int Atk, int Def, string Race, char Kind, Tile *parent): 
+Character(HP, Atk, Def,'@', parent), gold(0), Race(Race) {};
+
+string Player::getRace(){
+	return Race;
+}
 
 void Player::usePotion(Potion *p) {
     if (p->getType() == 'H') {
@@ -14,4 +21,17 @@ void Player::usePotion(Potion *p) {
         Def += p->getValue();
         
     }
+}
+
+void Player::getGold(Treasure *t){
+
+	if (Race == "dwarf"){ // double gold for dwarves ayyy
+		gold += 2*(t->getValue());
+
+	} else if ( Race == "orc"){ // half gold for orcs lmao
+		gold += 0.5*(t->getValue());
+
+	}else{ // rest of the races 
+		gold += t->getValue();
+	}
 }
--- player.h
+++ player.h
@@ -2,13 +2,17 @@
 #define _PLAYER_
 #include "character.h"
 #include "potion.h"
+#include "treasure.h"
 
-class Player: public  Character {
+class Player: public Character {
     int gold; 
+    std::string Race;
     public: 
-    Player(int HP, int Atk, int Def, char Kind, Tile *parent); 
+    Player(int HP, int Atk, int Def, std::string Race, char Kind, Tile *parent); 
     ~Player() = default; 
+    std::string getRace();
     virtual void usePotion(Potion *p);
+    virtual void getGold(Treasure *t);
 };
 
 #endif
